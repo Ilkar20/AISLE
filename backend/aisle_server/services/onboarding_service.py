f9f92f7789b1b4035d73d5651c718b24e2ad9f50
Yilikaer Yihamujiang <yilikaer.yihamujiang@tuni.fi>
2025-11-12 12:58:59 +0200

    added on Boarding service

---
+++ backend/aisle_server/services/onboarding_service.py
@@ -0,0 +1,8 @@
+# For now, this is a placeholder
+def create_user(username, language):
+    # In future: save to DB
+    return {
+        "user_id": 1,
+        "username": username,
+        "language": language
+    }
